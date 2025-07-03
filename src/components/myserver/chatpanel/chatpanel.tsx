e32dbdac3d9508b3592651537cd799e7f420d8d7
Evan Chen <cs.evanc@gmail.com>
2025-07-03 15:58:15 -0400

    fixed imports

--- src/components/myserver/chatpanel/chatpanel.tsx
+++ src/components/myserver/chatpanel/chatpanel.tsx
@@ -1,14 +1,14 @@
 import React, {useContext} from 'react'
 import { ChannelContext } from '../../contexts/channelContext'
-import HeaderBar from './header'
-import MessageArea from './messagearea'
+import HeaderBar from './header/header'
+import MessageArea from './messagearea/messagearea'
 
 const ChatPanel: React.FC = () => {
   const {currChannel} = useContext(ChannelContext)!
   return (
     <div className="flex flex-col flex-1">
       <HeaderBar currChannel = {currChannel.name}/>
-      {/*<MessageArea currChannel={currChannel} />*/}
+      <MessageArea currChannel={currChannel} />
     </div>
   )
 }
